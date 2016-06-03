51e2f3cd3b325f80629488714a009f66d4e8751c
i5ting <i5ting@126.com>
2016-06-04 07:36:16 +0800

    lazycommmit, please ignore

--- book/toc2.js
+++ book/toc2.js
@@ -5,21 +5,30 @@ require(["gitbook", "jQuery"], function(gitbook, $) {
       return gitbook.state.$book.hasClass('with-summary');
   }
   
-  $(window).bind('storage', function (e) {
-    alert(2)
-    a();
-  });
-  
-  gitbook.events.bind("page.change", function() {
-    a();
-  });
-  
-  
-  function a(){
-    if (isOpen() == false) {
-      $('.markdown-section > ul').first().show().addClass('toc2')
-    } else {
+  var i = 1;
+  $( window ).keydown(function( event ) {
+    console.log(event.which)
+    // enter = 13
+    // t = 84
+    if ( event.which === 13 || event.which === 84) {
+      if (i % 2 == 1) {
+        $('.markdown-section > ul').first().show().addClass('toc2')
+      } else {
+        $('.markdown-section > ul').first().hide()
+      }
+      
+      i++;
+    }
+    
+    // h = hide
+    if(event.which === 72){
       $('.markdown-section > ul').first().hide()
     }
-  }
+  });
+  
+  
+  gitbook.events.bind("page.change", function() {
+  
+  });
+  
 });
